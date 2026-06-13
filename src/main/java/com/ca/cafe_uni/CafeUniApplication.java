9ad4ea78a65ccea576f50f3ea829d643e2ebd828
Zack Allcca <zack.allcca@hotmail.com>
2026-06-12 20:55:30 -0500

    primer commit

---
+++ src/main/java/com/ca/cafe_uni/CafeUniApplication.java
@@ -0,0 +1,13 @@
+package com.ca.cafe_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeUniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ca/cafe_uni/CafeUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca.cafe_uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
